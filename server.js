f8284d3446e95a09fda7f8f84c402332fbb347bf
Muhammad Luqman Arshad <l230794@lhr.nu.edu.pk>
2025-03-23 16:30:29 +0500

    Added Post Requests with modifications to insertion procedures

--- server.js
+++ server.js
@@ -18,8 +18,6 @@ const reg_bus = express.urlencoded({
     parameterLimit: 6
 });
 
-
-
 app.get('/', function(req, res){
     sql.connect(config, function(err){
         if (err)
@@ -68,6 +66,190 @@ app.post("/register/business", reg_bus, (req, res) => {
         }
     })
 });
+
+const reg_man = express.urlencoded({ 
+    extended : false,
+    limit: 10000,
+    parameterLimit: 5
+});
+
+app.post("/register/manager", reg_man, (req, res) =>
+{
+    const {name, email, username, password, businessID} = req.body;
+    sql.connect(config, (err) => {
+        if (err) {
+            console.log(err);
+            res.status(505).json({ message: "Could not connect", err});
+        }
+        else
+        {
+            let request = new sql.Request();
+            request
+            .input("name", name)
+            .input("email", email)
+            .input("username", username)
+            .input("password", password)
+            .input("BusinessID", businessID)
+            .execute("insert_Managers", (err) => {
+                if (err)
+                {
+                    console.log(err);
+                    res.status(505).json({ message: "Could not execute query", err});
+                }
+                else
+                {
+                    res.json({message: "It was added successfully"});
+                }
+            });
+        }
+    })
+});
+
+const add_sto = express.urlencoded({ 
+    extended : false,
+    limit: 10000,
+    parameterLimit: 3
+});
+
+app.post("/add_store", add_sto, (req, res) =>
+{
+    const {name, businessID, address} = req.body;
+    sql.connect(config, (err) => {
+        if (err) {
+            console.log(err);
+            res.status(505).json({ message: "Could not connect", err});
+        }
+        else
+        {
+            let request = new sql.Request();
+            request
+            .input("StoreName", name)
+            .input("BusinessID", businessID)
+            .input("StoreAddress", address)
+            .execute("insert_Stores", (err) => {
+                if (err)
+                {
+                    console.log(err);
+                    res.status(505).json({ message: "Could not execute query", err});
+                }
+                else
+                {
+                    res.json({message: "It was added successfully"});
+                }
+            });
+        }
+    })
+});
+
+const add_pro = express.urlencoded({ 
+    extended : false,
+    limit: 10000,
+    parameterLimit: 4
+});
+
+app.post("/add_product", add_pro, (req, res) =>
+{
+    const {name, businessID, category, price} = req.body;
+    sql.connect(config, (err) => {
+        if (err) {
+            console.log(err);
+            res.status(505).json({ message: "Could not connect", err});
+        }
+        else
+        {
+            let request = new sql.Request();
+            request
+            .input("ProductName", name)
+            .input("BusinessID", businessID)
+            .input("Category", category)
+            .input("PricePerUnit", price)
+            .execute("insert_Products", (err) => {
+                if (err)
+                {
+                    console.log(err);
+                    res.status(505).json({ message: "Could not execute query", err});
+                }
+                else
+                {
+                    res.json({message: "It was added successfully"});
+                }
+            });
+        }
+    })
+});
+
+const add_inventory = express.urlencoded({ 
+    extended : false,
+    limit: 10000,
+    parameterLimit: 2
+});
+
+app.post("/add_inventory", add_inventory, (req, res) =>
+{
+    const {productID, warehouseID} = req.body;
+    sql.connect(config, (err) => {
+        if (err) {
+            console.log(err);
+            res.status(505).json({ message: "Could not connect", err});
+        }
+        else
+        {
+            let request = new sql.Request();
+            request
+            .input("ProductID", productID)
+            .input("WarehouseID", warehouseID)
+            .execute("insert_ProductinWarehouse", (err) => {
+                if (err)
+                {
+                    console.log(err);
+                    res.status(505).json({ message: "Could not execute query", err});
+                }
+                else
+                {
+                    res.json({message: "It was added successfully"});
+                }
+            });
+        }
+    })
+});
+
+const add_stock = express.urlencoded({ 
+    extended : false,
+    limit: 10000,
+    parameterLimit: 4
+});
+
+app.post("/add_stockreq", add_stock, (req, res) =>
+{
+    const {storeID, productID, quantity, message} = req.body;
+    sql.connect(config, (err) => {
+        if (err) {
+            console.log(err);
+            res.status(505).json({ message: "Could not connect", err});
+        }
+        else
+        {
+            let request = new sql.Request();
+            request
+            .input("ProductID", productID)
+            .input("RequestingStoreID", storeID)
+            .input("RequestedQuantity", quantity)
+            .input("message", message)
+            .execute("insert_StockRequests", (err) => {
+                if (err)
+                {
+                    console.log(err);
+                    res.status(505).json({ message: "Could not execute query", err});
+                }
+                else
+                {
+                    res.json({message: "It was added successfully"});
+                }
+            });
+        }
+    })
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
