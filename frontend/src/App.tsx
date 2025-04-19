0107d75fd35e1777b24303c596cdc9caf12916fc
Aamina-I <aaminaiftikharahmed@gmail.com>
2025-04-19 18:51:22 +0500

    Added page to Register Owner and Manager

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,8 @@ const Login = lazy(() => import('./Pages/Login'));
 const Owner = lazy(() => import('./Pages/owner'));
 const Manager = lazy(() => import('./Pages/Manager'));
 const NotFound = lazy(() => import('./Pages/NotFound'));
-const Logout = lazy(() => import("./Pages/Logout"))
+const Logout = lazy(() => import("./Pages/Logout"));
+const Register = lazy(() => import("./Pages/Register"))
 import ProtectedRoute from "./Components/ProtectedRoute";
 import Navbar from "./Components/navbar";
 import { getItem, setItem } from "./utils/localStorage";
@@ -30,6 +31,7 @@ function App() {
 						<Route path="/" element={<Home />} />
 						<Route path="/login" element={<Login setRole={setRole} />} />
 						<Route path='/logout' element={<Logout setRole={setRole} />} />
+						<Route path='/Register' element={<Register  />} />
 						<Route element={<ProtectedRoute allow='owner' />}>
 							<Route path="/owner" element={<Owner />} />
 							<Route path="/owner/stock_req" element={<Stock_Owner />} />
